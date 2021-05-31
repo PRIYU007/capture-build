5498ca2e0b3a513eb50da07a8d0873a617c223d5
PRIYU007 <technopriyu@gmail.com>
2021-05-31 15:00:22 +0530

    9. React Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import GlobalStyle from './components/GlobalStyle';
 
 // Import Pages
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
 import Nav from './components/Nav';
+
+// Router
+import { Switch, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      {/* Switch stops as it finds the first matching route*/}
+      <Switch>
+        {/* Route doesnt have to be exact. for eg  localhost:3000/work will render about as as it sees '/' then goes to next /work then render work.We use switch to make them stop.. */}
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
