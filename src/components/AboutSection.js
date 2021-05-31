3d0998d5569b0cbac2a913dd27d652dfcda323a1
PRIYU007 <technopriyu@gmail.com>
2021-05-31 11:54:57 +0530

    6.Styling Services Section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,7 @@
 import home1 from '../img/home1.png';
 // Styled
-import styled from 'styled-components';
+
+import { About, Description, Image, Hide } from '../styles';
 
 const AboutSection = () => {
   return (
@@ -33,35 +34,5 @@ const AboutSection = () => {
 };
 
 // Styled components
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 5rem 10rem;
-  color: white;
-`;
 
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    /* it stretch image back and keep the same aspect ratio, to check disbale it and check */
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 export default AboutSection;
