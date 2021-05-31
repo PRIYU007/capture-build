e430b93f1ae43c65020eb4ab36d70cd0ff8e6833
PRIYU007 <technopriyu@gmail.com>
2021-05-31 12:15:45 +0530

    7.Styling Faq Section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,44 +1,78 @@
+import styled from 'styled-components';
+import { About } from '../styles';
+
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any Questions <span>FAQ</span>
-        <div className="question">
-          <h4>How Do I Start?</h4>
-          <div className="answer">
-            <p>Learn/Teach Others</p>
-            <p>Start At The Beginning,Block Out Negative Noise,Just Do It!</p>
-          </div>
-        </div>
-        <div className="question">
-          <h4>Daily Schedule</h4>
-          <div className="answer">
-            <p>Start your day with your most important work.</p>
-            <p>
-              Map out your perfect daily schedule according to your personal
-              “productivity curve”
-            </p>
-          </div>
-        </div>
-        <div className="question">
-          <h4>Different Payment Methods</h4>
-          <div className="answer">
-            <p>We Accept</p>
-            <p>Paypal,Debit/Credit Card,Google Pay</p>
-          </div>
-        </div>
-        <div className="question">
-          <h4>What Products do you offer?</h4>
-          <div className="answer">
-            <p>Electronic Items</p>
-            <p>
-              Laptop,Mobile,Camera,TV,Voice Assistant,Smart Bulb,PC parts and
-              much more!
-            </p>
-          </div>
-        </div>
       </h2>
-    </div>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Learn/Teach Others</p>
+          <p>Start At The Beginning,Block Out Negative Noise,Just Do It!</p>
+        </div>
+        <div className="faq-line"></div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Start your day with your most important work.</p>
+          <p>
+            Map out your perfect daily schedule according to your personal
+            “productivity curve”
+          </p>
+        </div>
+        <div className="faq-line"></div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>We Accept</p>
+          <p>Paypal,Debit/Credit Card,Google Pay</p>
+        </div>
+        <div className="faq-line"></div>
+      </div>
+      <div className="question">
+        <h4>What Products do you offer?</h4>
+        <div className="answer">
+          <p>Electronic Items</p>
+          <p>
+            Laptop,Mobile,Camera,TV,Voice Assistant,Smart Bulb,PC parts and much
+            more!
+          </p>
+        </div>
+        <div className="faq-line"></div>
+      </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 export default FaqSection;
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -29,7 +29,7 @@ button{
         color: white;
     }
 }
-h2{
+ h2{
         font-weight:lighter;
         font-size:4rem;
     }
@@ -38,7 +38,7 @@ h2{
     }
     h4{
         font-weight:bold;
-        
+        font-size:2rem;
     }
     a{
         font-size:1.1rem;
