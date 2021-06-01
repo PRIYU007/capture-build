f0565d2de11d2a0ba01980906d992fa0f6aec330
PRIYU007 <technopriyu@gmail.com>
2021-06-01 15:53:05 +0530

    17. Reuseable Components

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,50 +1,58 @@
 import styled from 'styled-components';
 import { About } from '../styles';
-
+import Toggle from './Toggle';
 const FaqSection = () => {
   return (
     <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>Learn/Teach Others</p>
-          <p>Start At The Beginning,Block Out Negative Noise,Just Do It!</p>
+      <Toggle>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>Learn/Teach Others</p>
+            <p>Start At The Beginning,Block Out Negative Noise,Just Do It!</p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Start your day with your most important work.</p>
-          <p>
-            Map out your perfect daily schedule according to your personal
-            “productivity curve”
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>Daily Schedule</h4>
+          <div className="answer">
+            <p>Start your day with your most important work.</p>
+            <p>
+              Map out your perfect daily schedule according to your personal
+              “productivity curve”
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>We Accept</p>
-          <p>Paypal,Debit/Credit Card,Google Pay</p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>Different Payment Methods</h4>
+          <div className="answer">
+            <p>We Accept</p>
+            <p>Paypal,Debit/Credit Card,Google Pay</p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What Products do you offer?</h4>
-        <div className="answer">
-          <p>Electronic Items</p>
-          <p>
-            Laptop,Mobile,Camera,TV,Voice Assistant,Smart Bulb,PC parts and much
-            more!
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>What Products do you offer?</h4>
+          <div className="answer">
+            <p>Electronic Items</p>
+            <p>
+              Laptop,Mobile,Camera,TV,Voice Assistant,Smart Bulb,PC parts and
+              much more!
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
+      </Toggle>
     </Faq>
   );
 };
