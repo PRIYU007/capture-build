1f10eb56a4a5e0820f676d4028e66fe0147adcd2
PRIYU007 <technopriyu@gmail.com>
2021-06-01 14:25:59 +0530

    14.About Animation

--- src/animation.js
+++ src/animation.js
@@ -3,7 +3,30 @@ export const pageAnimation = {
   show: {
     opacity: 1,
     y: 0,
-    transition: { duration: 1 },
+    transition: { duration: 1, when: 'beforeChildren', staggerChildren: 0.25 },
   },
-  exit: { opacity: 0, y: 300, transition: { duration: 1 } },
+  exit: { opacity: 0, y: 300, transition: { duration: 0.5 } },
+};
+
+export const titleAnim = {
+  hidden: { y: 200 },
+  show: {
+    y: 0,
+    transition: { duration: 0.75, ease: 'easeOut' },
+  },
+};
+
+export const buttonAnim = {
+  hidden: { x: -200, opacity: 0 },
+  show: { x: 0, opacity: 1, transition: { duration: 0.3 } },
+};
+
+export const fade = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1, transition: { ease: 'easeOut', duration: 0.75 } },
+};
+
+export const photoAnim = {
+  hidden: { scale: 1.5, opacity: 0 },
+  show: { scale: 1, opacity: 1, transition: { ease: 'easeOut', duration: 1 } },
 };
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -2,6 +2,9 @@
 import { motion } from 'framer-motion';
 import { pageAnimation } from '../animation';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 const ContactUs = () => {
   return (
     <motion.div
@@ -12,6 +15,15 @@ const ContactUs = () => {
       style={{ background: '#fff' }}
     >
       <h1>Contact Us</h1>
+
+      <div className="icons">
+        <FontAwesomeIcon icon={faGithub} size="3x" />
+        <FontAwesomeIcon
+          icon={faTwitter}
+          size="3x"
+          style={{ color: '#1DA1F2' }}
+        />
+      </div>
     </motion.div>
   );
 };
